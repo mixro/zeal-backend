e0fd59e09cafc73fb4eaab2df0a2e6189db6c9d6
i CEP <josephchongola43@gmail.com>
2024-09-17 16:06:57 +0300

    modified:   routes/Order.js 	modified:   routes/verifyToken.js

--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -38,6 +38,16 @@ export const verifyTokenAndAuthorization = (req, res, next) => {
     })
 }
 
+export const verifyUserTokenAndAuthorization = (req, res, next) => {
+    verifyToken(req, res, () => {
+        if (req.user.id === req.params.userId || req.user.isAdmin) {
+            next();
+        } else {
+            res.status(403).json("You are not allowed to do that!");
+        }
+    })
+}
+
 export const verifyUserToken = (req, res, next) => {
     verifyToken(req, res, () => {
         if ( req.user.id === req.body.userId ) {
